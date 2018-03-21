30691fe5455b369426a91be0b7a73f73e1ab2fca
Blandified-PC\Blandified <blandified@gmail.com>
2018-03-20 18:56:14 -0700

    Initial commit of game files

---
+++ MM/MM/MM/GameLogicClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using Windows.Media.SpeechSynthesis;
+using Plugin.TextToSpeech;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NCalc;
+
+/* 
+ * This class handles all the logic. Computation, getting random numbers, etc. is all done here.
+ */
+
+namespace MM
+{
+    class GameLogicClass
+    {
+
+        //User-entered length of computation and user-entered maximum value passes into this method (delay currently unused). This method creates a list of random numbers with either a '+' or '-' 
+        //  in between each random number. The Speech function is used to read out this list so the user can hear. Method returns the calculated answer. 
+        public int LoopThroughComputation(int length, int range, int delay)
+        {
+            List<string> RandomNumberList_string = new List<string>();
+            string ConcatenatedString = string.Empty;
+
+            for (int i = 1; i <= length; i++)
+            {
+                int randomNumber = GetRandomNumber(1, range + 1);
+                RandomNumberList_string.Add(randomNumber.ToString());
+
+                if (i == length)
+                    RandomNumberList_string.Add("equals");
+                else
+                    RandomNumberList_string.Add(GetOperation());
+            }
+
+            foreach (string numberOrOperation in RandomNumberList_string)
+            {
+                if (numberOrOperation == "-")
+                    CrossTextToSpeech.Current.Speak("minus", speakRate: 1.25f);
+                else
+                    CrossTextToSpeech.Current.Speak(numberOrOperation, speakRate: 1.25f);
+
+                if (numberOrOperation != "equals")
+                    ConcatenatedString += numberOrOperation;
+            }
+
+            Expression e = new Expression(ConcatenatedString);
+            System.Diagnostics.Debug.WriteLine(e.Evaluate());
+            int Result = Convert.ToInt32(e.Evaluate());
+            return Result;
+
+        }
+
+        //Passes in user-entered answer and the correct answer determined from LoopThroughComputation. Returns true if they are the same. 
+        public bool IsAnswerCorrect(int userAnswerToCheck, int correctAnswer)
+        {
+            if (userAnswerToCheck == correctAnswer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Function to get operation (+, -)
+        public string GetOperation()
+        {
+            // 1 is + , 2 is -
+            int randomNumber = GetRandomNumber(1, 2 + 1);
+            if (randomNumber == 1)
+                return "+";
+            else if (randomNumber == 2)
+                return "-";
+            else
+                return "error";
+
+
+        }
+
+        //Function to get random number
+        private static readonly Random getrandom = new Random();
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getrandom) // synchronize {
+                return getrandom.Next(min, max);
+        }
+    }
+}
+
---
+++ MM/MM/MM/GamePage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+/* 
+ * This class controls the button presses from the XAML file and calls different functions/classes depending on which button is pressed.
+ */
+
+namespace MM
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GamePage : ContentPage
+    {
+
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+
+        //This is the return variable from GameLogic.LoopThroughComputation() that stores the answer from the random computation
+        public int correctAnswer;
+
+        //User presses this button after entering in values and this button starts the speaking
+        public void Btn_StartGame(object sender, EventArgs e)
+        {
+            //These are user-entered values, stores how long the computation is and what the max number will be
+            string x = LengthOfComputation.Text;
+            string y = RangeOfComputation.Text;
+
+            //TO DO
+            int Delay = 1;
+
+            //Check user input and make sure it is an int and greater than 0
+            if ((int.TryParse(x, out int Length)) && (Convert.ToInt32(x) > 0) && ((int.TryParse(y, out int Range)) && (Convert.ToInt32(y) > 0)))
+            {
+                GameLogicClass GameLogic = new GameLogicClass();
+                correctAnswer = GameLogic.LoopThroughComputation(Length, Range, Delay);
+            }
+            else
+            {
+                DisplayAlert("Error", "Please enter correct values", "OK");
+            }
+
+        }
+
+        //Button that user presses to check that their entered value is correct or not
+        public void Btn_CheckAnswer(object sender, EventArgs e)
+        {
+            //User input, what they think the answer is
+            string x = userAnswer.Text;
+
+            GameLogicClass GameLogic = new GameLogicClass();
+
+            //Check if user input is an int
+            if (int.TryParse(x, out int _userAnswer))
+            {
+                //Passes in the user input and checks if it is correct
+                bool correct = GameLogic.IsAnswerCorrect(Convert.ToInt32(_userAnswer), correctAnswer);
+
+                if (correct)
+                {
+                    System.Diagnostics.Debug.WriteLine("correct");
+                    DisplayAlert("Congratulations!", "That is correct!", "I got lucky... lemme try again");
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("wrong");
+                    DisplayAlert("WRONG!", "That is incorrect!", "I have failed... lemme try again");
+                }
+            }
+            else
+            {
+                DisplayAlert("Error", "Please enter correct values", "OK");
+            }
+        }
+    }
+}
